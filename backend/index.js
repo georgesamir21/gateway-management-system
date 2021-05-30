a5f2036362f3ccffd81c792fb339726bd653b475
georgesamir21 <georges.samir21@gmail.com>
2021-05-30 21:23:37 +0200

    connect to database

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,17 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+
+//connect to db...
+mongoose.connect(
+  `mongodb://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+)
+  .then(() => console.log('Connected to DB!'))
+  .catch((err) => console.log('Error Connect to DB!', err));
 
 const server = express();
 server.use(express.json());
