578a596c0d9173dd62bf714ea45d839ef06c844b
georgesamir21 <georges.samir21@gmail.com>
2021-05-30 23:34:48 +0200

    add database validation for the ip address

--- backend/models/gateways.js
+++ backend/models/gateways.js
@@ -4,21 +4,28 @@ module.exports = () => {
   const gatewaySchema = new Schema({
     name: {
       type: String,
-      required: true
+      required: true,
     },
     serial_number: {
       type: String,
       default: uuidv4,
       unique: true,
-      required: true
+      required: true,
     },
     ip_address: {
       type: String,
-      required: true
+      required: true,
+      validate: {
+        validator: (ip) =>
+          /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(
+            ip,
+          ),
+        message: (data) => `${data.value} is invalid IPV4 address!'`,
+      },
     },
-    devices: [{ type: Schema.Types.ObjectId, ref: 'Device' }]
+    devices: [{ type: Schema.Types.ObjectId, ref: 'Device' }],
   });
 
   const Gateway = model('Gateway', gatewaySchema);
   return Gateway;
-}
+};
